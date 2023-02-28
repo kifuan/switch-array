0ca312ff0c057d0cfe7cb31ac0e8cd2818d721f1
kifuan <kifuan@foxmail.com>
2023-02-28 22:28:26 +0800

    chore: update header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <header class="w-full h-16 bg-slate-50 flex justify-around">
-    <nuxt-link to="/" class="h-16 select-none leading-[4rem] text-2xl text-slate-800">
+    <nuxt-link to="/" class="h-16 cursor-pointer leading-[4rem] text-2xl text-slate-800">
       SwitchArray
     </nuxt-link>
 
     <div class="flex flex-row gap-x-5">
-      <a class="h-16 text-slate-800 leading-[4rem]" href="https://github.com/kifuan/switch-array" target="_blank">
+      <nuxt-link class="h-16 text-slate-800 leading-[4rem]" to="https://github.com/kifuan/switch-array" target="_blank">
         GitHub
-      </a>
+      </nuxt-link>
       <nuxt-link to="/about" class="h-16 text-slate-800 leading-[4rem]">
         About
       </nuxt-link>
